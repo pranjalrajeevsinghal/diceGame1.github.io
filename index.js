998b8b564279eb43a19c772802bbdd404654d309
pranjalrajeevsinghal <84920940+pranjalrajeevsinghal@users.noreply.github.com>
2022-11-24 15:35:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var randomNumber1 = Math.floor(((Math.random())*6))+1;
 var randomDiceImage = "dice" + randomNumber1 + ".png";
-var randomImageSource = "/images/" + randomDiceImage;
+var randomImageSource = "./images/" + randomDiceImage;
 document.querySelectorAll("img")[0].setAttribute("src",randomImageSource);
 var randomNumber2 = Math.floor(((Math.random())*6))+1;
 var randomDiceImage2 = "dice" + randomNumber2 + ".png";
-var randomImageSource2 = "/images/" + randomDiceImage2;
+var randomImageSource2 = "./images/" + randomDiceImage2;
 document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
 
 if(randomNumber1 > randomNumber2){
